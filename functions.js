51082d671ffd4a0d1770312c0a46984884264709
Jimz2245 <jimz7192@gmail.com>
2025-07-29 21:22:01 -0400

    asdasfd

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@ const addSong = (event) => {
     const Song = document.getElementById("Song").value;
     const Artist = document.getElementById("Artist").value;
 
-    const add = document.createElement("p");
+    const add = document.createElement("li");
     add.textContent = Name + "-" + Song + " by " + Artist; //puts the textContext (name, song, artist) into add
 
     const list = document.querySelector("#songList");
@@ -17,13 +17,37 @@ const nextSong = (event) => {
     event.preventDefault();
 
     const list = document.querySelector("#songList");
-    if (list.firstChild) 
+    const current = document.querySelector("#songNow");
+
+    const next = list.firstElementChild; // ensures we're targeting an actual element
+    const currentItem = current.firstElementChild;
+
+    if (next) 
     {
-        list.removeChild(list.firstChild);
+        if (currentItem) 
+        {
+            current.removeChild(currentItem); //removes the first child from songNow
+        }
+        current.appendChild(next); //moves the first child from songList to songNow
     } 
     else 
     {
         alert("No more songs in the queue.");
+    }
+}
+
+const clearSongs = (event) => {
+    event.preventDefault();
+
+    const list = document.querySelector("#songList");
+    const current = document.querySelector("#songNow");
+
+    while (list.firstChild) {
+        list.removeChild(list.firstChild); //removes all children from songList
+    }
+
+    while (current.firstChild) {
+        current.removeChild(current.firstChild); //removes all children from songNow
     }
 }
 
